f1bf4f6057ad1b49d46691b9c57cba37f5004b3d
Shahroz Khan <shahroz.yousfi@gmail.com>
2023-12-09 17:13:11 +0500

    chapter 4 documented ,code reformed and imports optimised

--- src/chapter4/programmingChallenges/solution.java
+++ src/chapter4/programmingChallenges/solution.java
@@ -8,6 +8,11 @@ import java.util.Scanner;
 import java.util.zip.ZipFile;
 
 public class solution {
+    /**
+     * Write a program that asks the user for a positive nonzero integer value. The program should
+     * use a loop to get the sum of all the integers from 1 up to the number entered. For example,
+     * if the user enters 50, the loop will find the sum of 1, 2, 3, 4, . . . 50.
+     */
     public static void solutionNo1() {
         Scanner scanner = new Scanner(System.in);
         System.out.println("Please enter a number");
@@ -20,6 +25,19 @@ public class solution {
         }
         System.out.println(sum);
     }
+
+    /**
+     * The distance a vehicle travels can be calculated as follows:
+     * Distance 5 Speed * Time
+     * For example, if a train travels 40 miles-per-hour for three hours, the distance traveled is
+     * 120 miles. Write a program that asks for the speed of a vehicle (in miles-per-hour) and the
+     * number of hours it has traveled. It should use a loop to display the distance a vehicle has
+     * traveled for each hour of a time period specified by the user. For example, if a vehicle is
+     * traveling at 40 mph for a three-hour time period, it should display a report similar to the
+     * one that follows:
+     * Input Validation: Do not accept a negative number for speed and do not accept any value
+     * less than 1 for time traveled.
+     */
     public static void solutionNo2(){
         Scanner scanner = new Scanner(System.in);
         System.out.println("Please enter the time in hours");
@@ -36,16 +54,30 @@ public class solution {
         }
         distance = speed * time;
         int distanceInAnHour = distance / time;
-        System.out.println("Hour                Distance Traveled");
+        System.out.println("Hour                 \t        Distance Traveled");
         for (int i = 1; i <= time ; i++) {
             System.out.println(i+"                      "+distanceInAnHour*i );
         }
     }
 
+    /**
+     * Modify the program you wrote for Programming Challenge 2 (Distance Traveled) so it
+     * writes the report to a file instead of the screen. Open the file in Notepad or another text
+     * editor to confirm the output.
+     */
+
     public static void solutionNo3(){
 
     }
 
+    /**
+     * Write a program that calculates the amount a person would earn over a period of time if his
+     * or her salary is one penny the first day, two pennies the second day, and continues to double
+     * each day. The program should display a table showing the salary for each day, and then
+     * show the total pay at the end of the period. The output should be displayed in a dollar
+     * amount, not the number of pennies.
+     * Input Validation: Do not accept a number less than 1 for the number of days worked.
+     */
     public static void solutionNo4(){
         Scanner scanner = new  Scanner(System.in);
         System.out.println("Please enter the no of days");
@@ -56,6 +88,12 @@ public class solution {
                 salary = salary * 2;
         }
     }
+
+    /**
+     * Write a program that asks the user to enter a string, and then asks the user to enter a character.
+     * The program should count and display the number of times that the specified character
+     * appears in the string.
+     */
     public static void solutionNo5(){
         Scanner scanner = new Scanner(System.in);
         System.out.println("Please enter a string");
@@ -70,9 +108,29 @@ public class solution {
         }
         System.out.println(character+" has appeared "+ j + " times in the string");
     }
+
+    /**
+     * Write a program that asks the user to enter the name of a file, and then asks the user to
+     * enter a character. The program should count and display the number of times that the
+     * specified character appears in the file. Use Notepad or another text editor to create a simple
+     * file that can be used to test the program.
+     */
     public static void solutionNo6(){
 
     }
+
+    /**
+     * A hotel’s occupancy rate is calculated as follows:
+     * Occupancy rate 5 Number of rooms occupied 4 Total number of rooms
+     * Write a program that calculates the occupancy rate for each floor of a hotel. The program
+     * should start by asking for the number of floors in the hotel. A loop should then iterate once
+     * for each floor. During each iteration, the loop should ask the user for the number of rooms
+     * on the floor and the number of them that are occupied. After all the iterations, the program
+     * should display the number of rooms the hotel has, the number of them that are occupied,
+     * the number that are vacant, and the occupancy rate for the hotel.
+     * Input Validation: Do not accept a value less than 1 for the number of floors. Do not accept
+     * a number less than 10 for the number of rooms on a floor.
+     */
 
     public static void solutionNo7(){
         Scanner scanner = new Scanner(System.in);
@@ -108,6 +166,17 @@ public class solution {
         System.out.println("Total no of vacant rooms = " + (totalNoOfRooms-totalNoOfOccupiedRooms));
         System.out.println("Occupancy rate = "+ totalNoOfOccupiedRooms/totalNoOfRooms);
     }
+
+    /**
+     * Write a program that uses nested loops to collect data and calculate the average rainfall
+     * over a period of years. First the program should ask for the number of years. The outer loop
+     * will iterate once for each year. The inner loop will iterate 12 times, once for each month.
+     * Each iteration of the inner loop will ask the user for the inches of rainfall for that month.
+     * After all iterations, the program should display the number of months, the total inches of
+     * rainfall, and the average rainfall per month for the entire period.
+     * Input Validation: Do not accept a number less than 1 for the number of years. Do not
+     * accept negative numbers for the monthly rainfall.
+     */
     public static void solutionNo8(){
         Scanner scanner = new Scanner(System.in);
         System.out.println("Please enter the number of years");
@@ -136,6 +205,18 @@ public class solution {
         System.out.println("Total inches of rainfall is "+ totalRainfall);
         System.out.println("Average inches of rainfall per month is "+ totalRainfall /(months * years));
     }
+
+    /**
+     * Write a program that will predict the size of a population of organisms. The program
+     * should ask for the starting number of organisms, their average daily population increase
+     * (as a percentage), and the number of days they will multiply. For example, a population
+     * might begin with two organisms, have an average daily increase of 50 percent, and will be
+     * allowed to multiply for seven days. The program should use a loop to display the size of
+     * the population for each day.
+     * Input Validation: Do not accept a number less than 2 for the starting size of the population.
+     * Do not accept a negative number for average daily population increase. Do not accept a
+     * number less than 1 for the number of days they will multiply.
+     */
     public static void solutionNo9(){
         Scanner scanner = new Scanner(System.in);
         double population = 0;
@@ -165,6 +246,12 @@ public class solution {
             System.out.println("The size of population on day "+ i + " is "+ population);
         }
     }
+
+    /**
+     *Write a program with a loop that lets the user enter a series of integers. The user should
+     * enter −99 to signal the end of the series. After all the numbers have been entered, the program
+     * should display the largest and smallest numbers entered.
+     */
     public static void solutionNo10(){
         Scanner scanner = new Scanner(System.in);
         int largest = 0;
@@ -186,11 +273,39 @@ public class solution {
         System.out.println("The largest value is " + largest);
         System.out.println("The smallest value is " + smallest);
     }
+
+    /**
+     * Write a program that displays a table of the Celsius temperatures 0 through 20 and
+     * their Fahrenheit equivalents. The formula for converting a temperature from Celsius to
+     * Fahrenheit is
+     * F=9/5 C + 32
+     * where F is the Fahrenheit temperature and C is the Celsius temperature. Your program must
+     * use a loop to display the table.
+     */
+
     public static void solutionNo11(){
         for (int i = 0; i <= 20; i++) {
             System.out.println(i+ " Degree = " + ((9/5) * i + 32) + " Fahrenheit");
         }
     }
+
+    /**
+     * Write a program that asks the user to enter today’s sales for five stores. The program should
+     * display a bar chart comparing each store’s sales. Create each bar in the bar chart by displaying
+     * a row of asterisks. Each asterisk should represent $100 of sales. Here is an example of
+     * the program’s output:
+     * Enter today's sales for store 1: 1000 [Enter]
+     * Enter today's sales for store 2: 1200 [Enter]
+     * Enter today's sales for store 3: 1800 [Enter]
+     * Enter today's sales for store 4: 800 [Enter]
+     * Enter today's sales for store 5: 1900 [Enter]
+     * SALES BAR CHART
+     * Store 1: **********
+     * Store 2: ************
+     * Store 3: ******************
+     * Store 4: ********
+     * Store 5: *******************
+     */
     public static void solutionNo12(){
         Scanner scanner = new Scanner(System.in);
         System.out.println("Enter today's sales for store 1");
@@ -203,21 +318,24 @@ public class solution {
         int store4 = scanner.nextInt();
         System.out.println("Enter today's sales for store 5");
         int store5 = scanner.nextInt();
-        for (int i = 0; i < 1; i++) {
 
-            System.out.println("Store 1 : ");
+
+
+            System.out.print("Store 1 : ");
             for (int j = 0; j < store1/100; j++) {
                 System.out.print("*");
             }
             System.out.println();
-        }
-        System.out.println("Store 2 : ");
-        for (int i = 0; i < 1; i++) {
+
+
+        System.out.print("Store 2 : ");
+
             for (int j = 0; j < store2/100; j++) {
                 System.out.print("*");
             }
             System.out.println();
-        }
+
+
         System.out.println("Store 3 : ");
         for (int i = 0; i < 1; i++) {
             for (int j = 0; j < store3/100; j++) {
@@ -240,6 +358,13 @@ public class solution {
             System.out.println();
         }
     }
+
+    /**
+     * Write a program that asks the user to enter the amount that he or she has budgeted for
+     * a month. A loop should then prompt the user to enter each of his or her expenses for the
+     * month, and keep a running total. When the loop finishes, the program should display the
+     * amount that the user is over or under budget.
+     */
     public static void solutionNo16(){
         Scanner scanner = new Scanner(System.in);
         System.out.println("Please enter your budget for this month");
@@ -259,25 +384,46 @@ public class solution {
             System.out.println("You are over budget");
         }
     }
+
+    /**
+     * Write a program that generates a random number and asks the user to guess what the
+     * number is. If the user’s guess is higher than the random number, the program should display
+     * “Too high, try again.” If the user’s guess is lower than the random number, the program
+     * should display “Too low, try again.” The program should use a loop that repeats until the
+     * user correctly guesses the random number.
+     */
     public static void solutionNo17(){
         Scanner scanner = new Scanner(System.in);
         System.out.println("Welcome to the number guessing game.\n The computer has randomly selected a number between 1 and 100");
         System.out.println("Take a guess until you succeed");
         int input = scanner.nextInt();
         int random = (int) (Math.random() * (100 - 1) + 1);
-        while(input != random){
-            if (input < random){
+
+        while(input != random) {
+
+
+            if (input < random) {
                 System.out.println("Too low try again");
-                input = scanner.nextInt();
-            } else if (input > random) {
+            } else {
                 System.out.println("Too high try again");
-                input = scanner.nextInt();
             }
+
+            System.out.println("Take a guess until you succeed");
+            input = scanner.nextInt();
+
         }
-        if (input == random){
-            System.out.println("Congratulations you got it");
-        }
+
+
+        System.out.println("Congratulations you got it");
+
+
     }
+
+    /**
+     * Enhance the program that you wrote for Programming Challenge 17 so it keeps a count
+     * of the number of guesses that the user makes. When the user correctly guesses the random
+     * number, the program should display the number of guesses.
+     */
     public static void solutionNo18(){
         Scanner scanner = new Scanner(System.in);
         System.out.println(" Welcome to the number guessing game.\n The computer has randomly selected a number between 1 and 100");
@@ -300,10 +446,29 @@ public class solution {
             System.out.println("You took "+ (i + 1) + " tries");
         }
     }
-    public static void solutionNo19(){
 
-    }
-    public static void solutionNo20() {
+    /**
+     * Write a program that asks the user for a positive integer no greater than 15. The program
+     * should then display a square on the screen using the character ‘X’. The number entered by
+     * the user will be the length of each side of the square. For example, if the user enters 5, the
+     * program should display the following:
+     * XXXXX
+     * XXXXX
+     * XXXXX
+     * XXXXX
+     * XXXXX
+     * If the user enters 8, the program should display the following:
+     * XXXXXXXX
+     * XXXXXXXX
+     * XXXXXXXX
+     * XXXXXXXX
+     * XXXXXXXX
+     * XXXXXXXX
+     * XXXXXXXX
+     * XXXXXXXX
+     */
+
+    public static void solutionNo19() {
         Scanner scanner = new Scanner(System.in);
         System.out.println("Please enter a number between 1 and 15");
         int num = scanner.nextInt();
@@ -312,6 +477,7 @@ public class solution {
             System.out.println("Please enter a number between 1 and 15");
             num = scanner.nextInt();
         }
+
         for (int i = 0; i < num; i++) {
             System.out.println();
             for (int j = 0; j < num; j++) {
@@ -319,7 +485,24 @@ public class solution {
             }
         }
     }
-    public static void solutionNo21(){
+
+    /**
+     * Write a program that plays a simple dice game between the computer and the user. When
+     * the program runs, a loop should repeat 10 times. Each iteration of the loop should do the
+     * following:
+     * • Generate a random integer in the range of 1 through 6. This is the value of the
+     * computer’s
+     * die.
+     * • Generate another random integer in the range of 1 through 6. This is the value of the
+     * user’s die.
+     * • The die with the highest value wins. (In case of a tie, there is no winner for that
+     * particular
+     * roll of the dice.)
+     * As the loop iterates, the program should keep count of the number of times the computer
+     * wins, and the number of times that the user wins. After the loop performs all of its iterations,
+     * the program should display who was the grand winner, the computer or the user.
+     */
+    public static void solutionNo20(){
         System.out.println("The Dice Game");
         int player = 0;
         int computer = 0;
@@ -347,7 +530,32 @@ public class solution {
             System.out.println("Computer");
         }
     }
-    public static void solutionNo22(){
+
+    /**
+     * A slot machine is a gambling device that the user inserts money into and then pulls a lever
+     * (or presses a button). The slot machine then displays a set of random images. If two or more
+     * of the images match, the user wins an amount of money that the slot machine dispenses
+     * back to the user.
+     * Create a program that simulates a slot machine. When the program runs, it should do
+     * the following:
+     * • Asks the user to enter the amount of money he or she wants to enter into the slot machine.
+     * • Instead of displaying images, the program will randomly select a word from the
+     * following
+     * list:
+     * Cherries, Oranges, Plums, Bells, Melons, Bars
+     * To select a word, the program can generate a random number in the range of 0
+     * through 5. If the number is 0, the selected word is Cherries; if the number is 1, the
+     * selected word is Oranges; and so forth. The program should randomly select a word
+     * from this list three times and display all three of the words.
+     * • If none of the randomly selected words match, the program will inform the user that
+     * he or she has won $0. If two of the words match, the program will inform the user
+     * that he or she has won two times the amount entered. If three of the words match, the
+     * program will inform the user that he or she has won three times the amount entered.
+     * • The program will ask whether the user wants to play again. If so, these steps are
+     * repeated. If not, the program displays the total amount of money entered into the slot
+     * machine and the total amount won.Decision Structures
+     */
+    public static void solutionNo21(){
         Scanner scanner = new Scanner(System.in);
         System.out.println("Please enter the amount of money you want to enter in the slot machine");
         int money = scanner.nextInt();
@@ -395,19 +603,19 @@ public class solution {
         }else if (spin3 == 5 ){
             System.out.println("Bars");
         }
+
         if (spin1 == spin2 && spin2 == spin3){
             winnings = winnings + (money * 3);
             System.out.println("You won " + money * 3);
         } else if (spin1 == spin2 || spin1 == spin3 || spin2 == spin3){
             winnings = winnings + (money * 2);
             System.out.println("You won " + money * 2);
-        }else {
-            winnings = winnings;
         }
+
         System.out.println("Enter y if you want to play again or enter n to end");
-        Character decision = scanner.next().charAt(0);
+        char decision = scanner.next().charAt(0);
         if (decision == 'y'){
-         solutionNo22();
+         solutionNo21();
         }else {
             System.out.println("Your total winnings are " + winnings);
         }
@@ -415,6 +623,6 @@ public class solution {
     }
 
     public static void main(String[] args) {
-        solutionNo22();
+        solutionNo12();
     }
 }
