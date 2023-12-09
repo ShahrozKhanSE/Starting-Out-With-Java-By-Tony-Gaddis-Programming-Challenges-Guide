b9025893c513f0af189d07d27c13d714c0c4a478
Shahroz Khan <shahroz.yousfi@gmail.com>
2023-12-09 13:37:55 +0500

    Chapter 1 completed documentation complete, code reformatted,imports optimised.

--- src/chapter1/programmingChallenges/solution.java
+++ src/chapter1/programmingChallenges/solution.java
@@ -3,6 +3,18 @@ package src.chapter1.programmingChallenges;
 public class solution {
 
     public static void main(String[] args) {
+        /**
+         * Here is
+         * the Java program you will enter:
+         * // This is my first Java program.
+         * public class MyFirstProgram
+         * {
+         * public static void main(String[] args)
+         * {
+         * System.out.println(“Hello World!”);
+         * }
+         * }
+         */
         System.out.println("Hello World`");
     }
 }
